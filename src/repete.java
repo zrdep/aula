8e08231752f2827ed19582d0edeb4ede039e75a9
Pedro Randolfo <pedrorandolfo.contato@gmail.com>
2026-05-28 19:47:01 -0500

    repete

---
+++ src/repete.java
@@ -0,0 +1,9 @@
+public class repete {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador <= 3) {
+            System.out.println("Olá " + contador);
+            contador++;
+        }
+    }
+}
